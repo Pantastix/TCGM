9f49bd3856a05066b354a644d989e81c6a88b572
chris <chrissimbeck@gmail.com>
2025-07-22 17:38:41 +0200

    new colors

--- composeApp/src/commonMain/kotlin/de/pantastix/project/ui/theme/Colors.kt
+++ composeApp/src/commonMain/kotlin/de/pantastix/project/ui/theme/Colors.kt
@@ -7,7 +7,7 @@ val PastelGreen = Color(0xFFAAFF93) // Unser helles Pastellgrün
 val DarkText = Color(0xFF1E1E1E)   // Für Text auf hellen Hintergründen
 
 // --- Dark Mode Hintergrund- und Oberflächenfarben ---
-val DarkBackground = Color(0xFF121212)    // Standard-Dunkelgrau für Hintergründe
-val DarkSurface = Color(0xFF1E1E1E)       // Etwas helleres Grau für Oberflächen (z.B. Karten)
+val DarkBackground = Color(0xFF1E1E1E)    // Standard-Dunkelgrau für Hintergründe
+val DarkSurface = Color(0xFF2E2E2E)       // Etwas helleres Grau für Oberflächen (z.B. Karten)
 val LightText = Color(0xFFE0E0E0)        // Helles Grau für Text auf dunklen Hintergründen
 val MediumGray = Color(0xFF424242)       // Ein neutrales Grau für sekundäre Elemente
